c445d334745e625f0b77bef14becc4cb33b6abe6
peache-1910 <peache_1910@icloud.com>
2022-05-08 14:16:57 +0900

    name&ageデータ変更

--- peache/src/peache/Training.java
+++ peache/src/peache/Training.java
@@ -5,6 +5,10 @@ public class Training {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
        System.out.println("ようこそ赤塚の世界へ");
+     int age= 28;
+     String name ="ひらき";
+     
+    		  
 	}
 
 }
