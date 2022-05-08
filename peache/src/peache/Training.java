103c82d32624830bedf5e685e1adaa33320e3e14
peache-1910 <peache_1910@icloud.com>
2022-05-08 19:27:25 +0900

    新システム導入

--- peache/src/peache/Training.java
+++ peache/src/peache/Training.java
@@ -20,7 +20,16 @@ public class Training {
      Add a = new Add();
      int total = a.add(10, 20);
      
-     System.out.print(total);
+     System.out.println(total);
+     
+     System.out.println("誕生石検索！自分の誕生日をご入力ください。");
+     String name1 ="ヒラキ";
+     int mans1 = 1;
+    
+     String[] birthday = {"ガーネット","アメジスト" ,"アクアマリン" ,"ダイヤモンド" ,"エメラルド" ,"パール" ,"ルビー" ,"ペリドット" ,"サファイヤ" ,"オパール" ,"トパーズ" ,"ターコイズ"};
+     int i = mans1 - 1;
+    
+     System.out.println(name1 + "さんは" + mans1 + "月の生まれ 、なので誕生石は" + birthday[i] + "になります。");
 	}
 
 }
